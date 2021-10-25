280c06e265e8313308f4690ad96b63fc18b971b8
Jamal <jamalabdul47@gmail.com>
2021-10-25 15:12:23 -0400

    Updated syntax

--- extension.js
+++ extension.js
@@ -13,11 +13,21 @@ function activate(context) {
 			const selection = editor.selection;
 			// returns text from specific line
 			// next time work on iteration
-			console.log(document.lineAt(1).text);
-
-			vscode.window.activeTextEditor.edit(editBuider => {
-
-			})
+			console.log(document.lineCount);
+			const word = document.getText(selection);
+			const reversed = word.split('').reverse().join('');
+			editor.edit(editBuilder => {
+				editBuilder.delete(selection)
+				})
+			// for(let i = 0; document.lineCount >= i; i++){
+			// 	console.log(document.lineAt(i).text);
+			// 	if(document.lineAt(i).includes('console.log')){
+			// 		vscode.window.editor.edit	editBuilder => {
+			// 			editBuilder.delete(0, document.lineAt(i))
+			// 		})
+			// 	}
+			// }
+			
 
 		}else{
 			console.log('editor not present')
